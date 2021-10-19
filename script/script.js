afafe3c68d193725e23424fc0e1285c28aebba60
Yuliya Babitskaya <bambina-kissi@yandex.ru>
2021-10-19 23:18:23 +0300

    first commit

---
+++ script/script.js
@@ -0,0 +1,10 @@
+// let input = document.querySelector('#form_url');
+
+// input.oninvalid = function(event) {
+//     event.target.setCustomValidity('Не соответствует формату');
+// }
+// let inputPhone = document.querySelector('#form_url2');
+
+// inputPhone.oninvalid = function(event) {
+//     event.target.setCustomValidity('Не соответствует формату');
+// }
