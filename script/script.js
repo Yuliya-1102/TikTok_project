6ead43d7ce8970d580942e7aabd0a09bac52da03
Yuliya Babitskaya <bambina-kissi@yandex.ru>
2021-10-22 19:45:14 +0300

    adding information

--- script/script.js
+++ script/script.js
@@ -1,10 +1,65 @@
-// let input = document.querySelector('#form_url');
+'use strict';
 
-// input.oninvalid = function(event) {
-//     event.target.setCustomValidity('Не соответствует формату');
-// }
-// let inputPhone = document.querySelector('#form_url2');
+const form = document.querySelector('#form');
+const formInput = document.querySelector('#form_url');
 
-// inputPhone.oninvalid = function(event) {
-//     event.target.setCustomValidity('Не соответствует формату');
-// }
+var tt;
+var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+var request = new XMLHttpRequest();
+
+function loadComplete(evt) {
+    tt = JSON.parse(request.responseText);
+    console.log(tt.video);
+    return tt;
+} 
+
+function loadData() {
+    request.open('POST', 'https://t3hverm.ru');
+    request.setRequestHeader("Content-Type", "application/json");
+    request.onload = loadComplete;
+    var data = JSON.stringify({"url": formInput.value});
+    request.send(data);
+}
+
+//ФУНКЦИЯ ОБРАБАТЫВАЕТ НАЖАТИЕ КНОПКИ
+form.addEventListener('submit', (event) => {
+    event.preventDefault();
+
+    loadData();
+
+    cross_download(url - то, что должны получить в loadData(), 'file')
+});
+
+//ФУНКЦИЯ ЗАБИРАЕТ ССЫЛКУ, ЧТО ПОЛУЧИЛИ НА СЕРВЕРЕ  И СКАЧИВВЕТ ФАЙЛ НА КОМПЬЮТЕР ПОЛЬЗОАТЕЛЮ
+function cross_download(url, fileName) {
+    var req = new XMLHttpRequest();
+    req.open("GET", url, true);
+    req.responseType = "blob";
+    var __fileName = fileName;
+    req.onload = function (event) {
+        var blob = req.response;
+        var contentType = req.getResponseHeader("content-type");
+        if (window.navigator.msSaveOrOpenBlob) {
+            // Internet Explorer
+            window.navigator.msSaveOrOpenBlob(new Blob([blob], {type: contentType}), fileName);
+        } else {
+            var link = document.createElement('a');
+            document.body.appendChild(link);
+            link.download = __fileName;
+            link.href = window.URL.createObjectURL(blob);
+            link.click();
+            document.body.removeChild(link); //remove the link when done
+        }
+    };
+    req.send();
+}
+
+function get_file_url(url) {
+	var link_url = document.createElement("a");
+	link_url.download = url.substring((url.lastIndexOf("/") + 1), url.length);
+	link_url.href = url;
+	document.body.appendChild(link_url);
+	link_url.click();
+	document.body.removeChild(link_url);
+	// delete link_url;
+}
